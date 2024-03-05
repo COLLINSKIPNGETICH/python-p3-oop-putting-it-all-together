d2996c53599896fb5a09e2ea7fa26e9ace14c8d9
+collins <+collinskipngetich60@gmail.com>
2024-03-05 16:16:56 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
+# book.py
+
 class Book:
-    pass
+    def __init__(self, title, page_count, price):  # Add the 'price' parameter
+        self.title = title
+        self.page_count = page_count
+        self.price = price  # Set the 'price' attribute
+        self.current_page = 1  # Initialize current_page attribute
+
+    def turn_page(self):
+        self.current_page += 1
+        return "Flipping the page...wow, you read fast!"
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, model, size, price):
+        self.brand = brand
+        self.model = model
+        self.size = size
+        self.price = price
+        self._discount = 0
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if 0 <= value <= 1:
+            self._discount = value
+        else:
+            raise ValueError("Discount must be between 0 and 1")
+
+    def apply_discount(self):
+        discounted_price = self.price * (1 - self.discount)
+        return round(discounted_price, 2)
+
